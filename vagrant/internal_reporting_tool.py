621c927593e6496f9fef01e53b7bf626b9177cee
Charles Brands <charles.brands@gmail.com>
2017-08-20 21:13:40 +0200

    code to connect to database and run a basic query

--- vagrant/internal_reporting_tool.py
+++ vagrant/internal_reporting_tool.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#! /usr/bin/env python3
 
 """
 internal_reporting_tool is a tool to create reports out of a news database.
@@ -7,5 +7,28 @@ internal_reporting_tool is a tool to create reports out of a news database.
 #import PostgreSQL driver
 import psycopg2
 
+DB_NAME = "news"
+
+def connect(database_name="news"):
+    """Connects to the database and returns a database connection."""
+    try:
+        db = psycopg2.connect(database=DB_NAME)
+        c = db.cursor()
+        return db, c
+    except psycopg2.Error:
+        print ("Error connecting to the database")
+        exit(1)
+
+
+def get_query_results(query):
+    db, c = connect()
+    c.execute(query)
+    results = c.fetchall()
+    db.close()
+    return results
+
 if __name__=='__main__':
-	print("Hello world")
+    print("testing database connection and get_query_results")
+    results = get_query_results("select * from articles limit 10")
+    for result in results:
+        print(result)
