f076dd6ff3a77a5c8fd68f1d3c7fd297e3f779f8
Charles Brands <charles.brands@gmail.com>
2017-08-20 19:11:43 +0200

    added python file

---
+++ vagrant/internal_reporting_tool.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+"""
+internal_reporting_tool is a tool to create reports out of a news database.
+"""
+
+#import PostgreSQL driver
+import psycopg2
+
+if __name__=='__main__':
+	print("Hello world")
