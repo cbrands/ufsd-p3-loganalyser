522958d8b80c334e7b49897c6d5bbd3a7b1ad9af
Charles Brands <charles.brands@gmail.com>
2017-08-21 20:54:49 +0200

    extracted database manager

---
+++ vagrant/db_manager.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python3
+
+import psycopg2
+
+class DbManager():
+    """
+    This class manages the database connection and runs queries
+    """
+    def __init__(self, db_name):
+        """
+        Initializes DbManager
+        db_name: The name of the database
+        """
+        self.db_name = db_name
+    
+    
+    def connect(self, database_name="news"):
+        """Connects to the database and returns a database connection."""
+        try:
+            db = psycopg2.connect(database=self.db_name)
+            c = db.cursor()
+            return db, c
+        except psycopg2.Error:
+            print ("Error connecting to the database")
+            exit(1)
+
+
+    def get_query_results(self, query):
+        """Open connection, execute query, and return the results"""
+        db, c = self.connect()
+        c.execute(query)
+        results = c.fetchall()
+        db.close()
+        return results
--- vagrant/internal_reporting_tool.py
+++ vagrant/internal_reporting_tool.py
@@ -4,34 +4,38 @@
 internal_reporting_tool is a tool to create reports out of a news database.
 """
 
-#import PostgreSQL driver
-import psycopg2
+import db_manager
 
 DB_NAME = "news"
 
-def connect(database_name="news"):
-    """Connects to the database and returns a database connection."""
-    try:
-        db = psycopg2.connect(database=DB_NAME)
-        c = db.cursor()
-        return db, c
-    except psycopg2.Error:
-        print ("Error connecting to the database")
-        exit(1)
 
 
-def get_query_results(query):
-    """Open connection, execute query, and return the results"""
-    db, c = connect()
-    c.execute(query)
-    results = c.fetchall()
-    db.close()
-    return results
+#def connect(database_name="news"):
+#    """Connects to the database and returns a database connection."""
+#    try:
+#        db = psycopg2.connect(database=DB_NAME)
+#        c = db.cursor()
+#        return db, c
+#    except psycopg2.Error:
+#        print ("Error connecting to the database")
+#        exit(1)
+#
+#
+#def get_query_results(query):
+#    """Open connection, execute query, and return the results"""
+#    db, c = connect()
+#    c.execute(query)
+#    results = c.fetchall()
+#    db.close()
+#    return results
+
+
 
 if __name__=='__main__':
     print("testing query3")
     #results = get_query_results("select * from popular_article_view limit 3")
     #results = get_query_results("select * from popular_authors_view")
-    results = get_query_results("select date, percent_error from error_log_view")
+    db = db_manager.DbManager(DB_NAME)
+    results = db.get_query_results("select date, percent_error from error_log_view")
     for result in results:
         print(result)
